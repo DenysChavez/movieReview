818ea1f0223cd47f8440279948fefeafc53390b8
Denys <go4denys@gmail.com>
2023-02-19 19:25:54 -0500

    fix @media grid

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=908d3d121803fb026aaa9f0bd02e1d2b&query=";
 
-const main = document.getElementById("section");
+const main = document.getElementById("movies-section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
+const movies_grid = document.getElementById("movies-grid");
 
 returnMovies(APILINK);
 
@@ -12,14 +13,15 @@ function returnMovies(url) {
     fetch(url).then(res => res.json()).then(function (data) {
         console.log(data.results);
         data.results.forEach(element => {
+            console.log(element);
             const div_card = document.createElement("div");
             div_card.setAttribute("class", "card");
 
-            const div_row = document.createElement("div");
-            div_card.setAttribute("class", "row");
+            const div_movie = document.createElement("a");
+            div_card.setAttribute("class", "movie");
 
-            const div_column = document.createElement("div");
-            div_column.setAttribute("class", "column");
+            // const div_column = document.createElement("div");
+            // div_column.setAttribute("class", "column");
 
             const image = document.createElement("img");
             image.setAttribute("class", "thumbnail");
@@ -27,17 +29,29 @@ function returnMovies(url) {
 
             const title = document.createElement("h3");
             title.setAttribute("id", "title");
+            title.setAttribute("class", "movie-title");
 
-            const center = document.createElement("center");
+            const reviews = document.createElement("a");
+            reviews.setAttribute("class", "reviews")
             
+
+            div_card.appendChild(div_movie);
+            div_card.appendChild(reviews)
+            div_movie.appendChild(image);
+            div_movie.appendChild(title);
+            movies_grid.appendChild(div_card);
+            // center.appendChild(image);
+            // div_card.appendChild(center);
+            // div_card.appendChild(title);
+            // div_column.appendChild(div_card);
+            // div_row.appendChild(div_column);
+            // main.appendChild(div_row);
+
             title.innerHTML = `${element.title}`;
+            reviews.innerHTML = "Reviews";
+            reviews.href = `movie.html?${element.id}&title=${element.title}`;
+            // reviews.setAttribute("href", `movie.html?${element.id}&title=${element.title}`);
             image.src = IMG_PATH + element.poster_path;
-            center.appendChild(image);
-            div_card.appendChild(center);
-            div_card.appendChild(title);
-            div_column.appendChild(div_card);
-            div_row.appendChild(div_column);
-            main.appendChild(div_row);
         })
     }) 
 }
